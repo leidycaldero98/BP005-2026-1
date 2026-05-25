82f697a9691512642b104bec329924d40748dfe6
leidycaldero98 <roindira13@gmail.com>
2026-05-25 18:39:45 -0500

    Create ejemplo_06_funcion_mostrar_arreglo.c

---
+++ Parcial2/punteroarray.c/ejemplo_06_funcion_mostrar_arreglo.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+void mostrarArreglo(intdatos[], intn) { 
+int i; 
+for(i=0;i<n;i++){ 
+printf("%d",datos[i]); } 
+printf("\n"); } intmain(){ 
+int datos[5]={8,3,15,6,10}; 
+mostrarArreglo(datos,5); 
+return0; 
+}
