744fdcdb11d03c2ac9a8c29b8fba41063c9dd4b4
leidycaldero98 <roindira13@gmail.com>
2026-05-25 18:37:21 -0500

    Create ejemplo_01_arreglo_basico.c

---
+++ Parcial2/punteroarray.c/ejemplo_01_arreglo_basico.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+intmain(){ /*Declaramosunarreglode cincoenteros.*/ 
+  intdatos[5]={8,3,15,6,10}; /*Mostramoscadaelementousandosuindice.*/ 
+  printf("datos[0]= %d\n",datos[0]); 
+  printf("datos[1]= %d\n",datos[1]); 
+  printf("datos[2]= %d\n",datos[2]); 
+  printf("datos[3]= %d\n",datos[3]); 
+  printf("datos[4]= %d\n",datos[4]); 
+  return0; }
