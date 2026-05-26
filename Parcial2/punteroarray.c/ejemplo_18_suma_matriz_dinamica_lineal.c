996e625c90a2627683af12d3957b1e860459e33b
leidycaldero98 <roindira13@gmail.com>
2026-05-25 19:15:51 -0500

    Create ejemplo_18_suma_matriz_dinamica_lineal.c

---
+++ Parcial2/punteroarray.c/ejemplo_18_suma_matriz_dinamica_lineal.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumarMatriz(int *matriz, int filas, int columnas) {
+
+    int i;
+    int j;
+
+    int suma = 0;
+
+    if (matriz == NULL) {
+        return 0;
+    }
+
+    for (i = 0; i < filas; i++) {
+
+        for (j = 0; j < columnas; j++) {
+            suma = suma + matriz[i * columnas + j];
+        }
+    }
+
+    return suma;
+}
+
+int main() {
+
+    int filas = 2;
+    int columnas = 3;
+
+    int i;
+    int j;
+
+    int *matriz = NULL;
+
+    matriz = malloc(filas * columnas * sizeof(int));
+
+    if (matriz == NULL) {
+        printf("No se pudo reservar memoria.\n");
+        return 1;
+    }
+
+    for (i = 0; i < filas; i++) {
+
+        for (j = 0; j < columnas; j++) {
+            matriz[i * columnas + j] = i * columnas + j + 1;
+        }
+    }
+
+    printf("Suma = %d\n",
+           sumarMatriz(matriz, filas, columnas));
+
+    free(matriz);
+    matriz = NULL;
+
+    return 0;
+}
