05bdf6e0bd84608d7f6b1c83014d70d19fda03ef
leidycaldero98 <roindira13@gmail.com>
2026-05-25 19:14:11 -0500

    Create ejemplo_15_funcion_mostrar_matriz.c

---
+++ Parcial2/punteroarray.c/ejemplo_15_funcion_mostrar_matriz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define COLUMNAS 3
+
+void mostrarMatriz(int m[][COLUMNAS], int filas) {
+
+    int i;
+    int j;
+
+    for (i = 0; i < filas; i++) {
+
+        for (j = 0; j < COLUMNAS; j++) {
+            printf("%d ", m[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int main() {
+
+    int m[2][COLUMNAS] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    mostrarMatriz(m, 2);
+
+    return 0;
+}
