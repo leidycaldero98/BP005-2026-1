c3b8e573847ede122b4e2513212dca4988180c2b
leidycaldero98 <roindira13@gmail.com>
2026-05-25 22:05:30 -0500

    Create figura_12_reloj_arena.cpp

---
+++ Parcial2/loopsfiguras.cpp/figura_12_reloj_arena.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+int main() {
+
+    int fila = 5;
+
+    int espacios, simbolos;
+
+    while (fila >= 1) {
+
+        espacios = 5 - fila;
+
+        while (espacios > 0) {
+
+            std::cout << " ";
+
+            espacios--;
+        }
+
+        simbolos = 2 * fila - 1;
+
+        while (simbolos > 0) {
+
+            std::cout << "@";
+
+            simbolos--;
+        }
+
+        std::cout << std::endl;
+
+        fila--;
+    }
+
+    fila = 2;
+
+    while (fila <= 5) {
+
+        espacios = 5 - fila;
+
+        while (espacios > 0) {
+
+            std::cout << " ";
+
+            espacios--;
+        }
+
+        simbolos = 2 * fila - 1;
+
+        while (simbolos > 0) {
+
+            std::cout << "@";
+
+            simbolos--;
+        }
+
+        std::cout << std::endl;
+
+        fila++;
+    }
+
+    return 0;
+}
