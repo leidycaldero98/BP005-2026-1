8f5ba361f0be1e94460630485c62fc715e7decba
leidycaldero98 <roindira13@gmail.com>
2026-05-25 22:05:51 -0500

    Create figura_13_triangulo_numerico.cpp

---
+++ Parcial2/loopsfiguras.cpp/figura_13_triangulo_numerico.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main() {
+
+    int fila, espacios, numero;
+
+    int n = 5;
+
+    for (fila = 1; fila <= n; fila++) {
+
+        for (espacios = 1;
+             espacios <= n - fila;
+             espacios++) {
+
+            std::cout << " ";
+        }
+
+        for (numero = 1;
+             numero <= fila;
+             numero++) {
+
+            std::cout << " "
+                      << numero
+                      << " ";
+        }
+
+        for (numero = fila - 1;
+             numero >= 1;
+             numero--) {
+
+            std::cout << " "
+                      << numero
+                      << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
