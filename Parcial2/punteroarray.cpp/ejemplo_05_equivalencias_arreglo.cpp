1b117e44c73c4cfaa9fe800d52d84444e0373e92
leidycaldero98 <roindira13@gmail.com>
2026-05-25 19:22:06 -0500

    Create ejemplo_05_equivalencias_arreglo.cpp

---
+++ Parcial2/punteroarray.cpp/ejemplo_05_equivalencias_arreglo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    int datos[3] = {10, 20, 30};
+
+    int *p = datos;
+
+    std::cout << "datos[1] = "
+              << datos[1] << std::endl;
+
+    std::cout << "*(datos + 1) = "
+              << *(datos + 1) << std::endl;
+
+    std::cout << "*(p + 1) = "
+              << *(p + 1) << std::endl;
+
+    std::cout << "p[1] = "
+              << p[1] << std::endl;
+
+    return 0;
+}
