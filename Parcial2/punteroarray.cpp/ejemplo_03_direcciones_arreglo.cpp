5a029cf5ef8219d476e783e8abd70b536628e62f
leidycaldero98 <roindira13@gmail.com>
2026-05-25 19:21:17 -0500

    Create ejemplo_03_direcciones_arreglo.cpp

---
+++ Parcial2/punteroarray.cpp/ejemplo_03_direcciones_arreglo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+    int datos[5] = {8, 3, 15, 6, 10};
+
+    int i;
+
+    for (i = 0; i < 5; i++) {
+
+        std::cout << "datos[" << i << "] = "
+                  << datos[i]
+                  << ", direccion = "
+                  << static_cast<const void *>(&datos[i])
+                  << std::endl;
+    }
+
+    return 0;
+}
