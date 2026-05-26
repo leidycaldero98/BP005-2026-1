44485a31c1fbaef62347c0b7f04dd1cb3096411a
leidycaldero98 <roindira13@gmail.com>
2026-05-25 22:02:27 -0500

    Create figura_04_piramide_for.cpp

---
+++ Parcial2/loopsfiguras.cpp/figura_04_piramide_for.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    int fila, espacios, simbolos;
+
+    for (fila = 1; fila <= 5; fila++) {
+
+        for (espacios = 1;
+             espacios <= 5 - fila;
+             espacios++) {
+
+            std::cout << " ";
+        }
+
+        for (simbolos = 1;
+             simbolos <= 2 * fila - 1;
+             simbolos++) {
+
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
