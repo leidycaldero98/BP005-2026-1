274dbb80e0d635fe822f37a1469e5fb422829e54
leidycaldero98 <roindira13@gmail.com>
2026-05-25 21:43:35 -0500

    Create ejemplo_03_operador_contenido.cpp

---
+++ Parcial2/puntero.cpp/ejemplo_03_operador_contenido.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+    int x = 25;
+
+    int *p = &x;
+
+    std::cout << "x = "
+              << x << std::endl;
+
+    std::cout << "*p = "
+              << *p << std::endl;
+
+    *p = 99;
+
+    std::cout << "x despues = "
+              << x << std::endl;
+
+    return 0;
+}
