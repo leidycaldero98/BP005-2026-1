2516bd72bc909b34cc5c379825f0c581d4caf96a
leidycaldero98 <roindira13@gmail.com>
2026-05-25 22:05:08 -0500

    Create figura_11_rombo_hueco.cpp

---
+++ Parcial2/loopsfiguras.cpp/figura_11_rombo_hueco.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() {
+
+    int fila, columna;
+
+    int n = 5;
+
+    int ancho = 2 * n - 1;
+
+    for (fila = 1;
+         fila <= ancho;
+         fila++) {
+
+        int distancia =
+            fila <= n ?
+            n - fila :
+            fila - n;
+
+        for (columna = 1;
+             columna <= ancho;
+             columna++) {
+
+            if (columna == distancia + 1 ||
+                columna == ancho - distancia) {
+
+                std::cout << "#";
+
+            } else {
+
+                std::cout << " ";
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
