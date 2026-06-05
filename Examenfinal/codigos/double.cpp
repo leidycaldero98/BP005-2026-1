7fa2a0c610491f3e186312a7a1f3e176696759cd
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:32:36 -0500

    Create double.cpp

---
+++ Examenfinal/codigos/double.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Incluye la librería para mostrar datos en consola.
+
+using namespace std; // Permite usar cout y endl directamente.
+
+int main() { // Inicia la función principal.
+
+    double temperatura = 24.5;
+    double distancia = 1.75;
+    double voltaje = 3.3;
+
+    cout << temperatura << endl;
+    cout << distancia << endl;
+    cout << voltaje << endl;
+
+    return 0;
+}
