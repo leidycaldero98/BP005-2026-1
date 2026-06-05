9c13ac733fbfb12a0909564f5c0ee00cbbfc9b37
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:31:52 -0500

    Create numeric_limits.cpp

---
+++ Examenfinal/codigos/numeric_limits.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // Permite imprimir en consola.
+#include <limits> // Incluye numeric_limits.
+#include <cstdint> // Incluye tipos de tamaño definido.
+
+using namespace std; // Permite usar cout y endl directamente.
+
+int main() { // Inicia la función principal.
+
+    cout << "int min: " << numeric_limits<int>::min() << endl;
+    cout << "int max: " << numeric_limits<int>::max() << endl;
+
+    cout << "uint8_t max: "
+         << static_cast<int>(numeric_limits<uint8_t>::max()) << endl;
+
+    cout << "int16_t min: " << numeric_limits<int16_t>::min() << endl;
+    cout << "int16_t max: " << numeric_limits<int16_t>::max() << endl;
+
+    return 0;
+}
