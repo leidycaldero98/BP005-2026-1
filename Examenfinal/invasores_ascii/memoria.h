38e2bc4e3f438f7b235274f6cc99af5402e29ed0
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:21:32 -0500

    Create memoria.h

---
+++ Examenfinal/invasores_ascii/memoria.h
@@ -0,0 +1,10 @@
+#ifndef MEMORIA_H
+#define MEMORIA_H
+
+#include "entidades.h"
+#include "juego.h"
+
+
+void mostrarReporteMemoria(const Nave* n, const Bala balas[], int tamB, const Enemigo enemigos[], int tamE, const Juego* j);
+
+#endif
