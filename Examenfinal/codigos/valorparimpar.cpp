bdc36a3273948cf80f4fb32994af9ab59bcc80ec
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:45:34 -0500

    Create valorparimpar.cpp

---
+++ Examenfinal/codigos/valorparimpar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int numero;
+
+    cout << "Ingrese un número entero: ";
+
+    cin >> numero;
+
+    if (numero % 2 == 0) {
+
+        cout << "El número es par" << endl;
+
+    } else {
+
+        cout << "El número es impar" << endl;
+    }
+
+    return 0;
+}
