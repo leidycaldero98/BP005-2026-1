4a3a1b48cda95146261e05ac38442003ca8992ca
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:32:14 -0500

    Create ejemploenteros.cpp

---
+++ Examenfinal/codigos/ejemploenteros.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Incluye la librería para usar cout y endl.
+
+using namespace std; // Permite escribir cout y endl sin anteponer std::.
+
+int main() { // Inicia la función principal del programa.
+
+    int edad = 18; // Declara una variable entera llamada edad y guarda 18.
+    int estudiantes = 35; // Declara una variable entera y guarda 35.
+    int contador = 0; // Declara una variable entera que inicia en 0.
+
+    cout << edad << endl;
+    cout << estudiantes << endl;
+    cout << contador << endl;
+
+    return 0;
+}
