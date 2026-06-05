a21dd4c5c1cb9ee9ec86e9ac9dea4fb3eade0506
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:35:48 -0500

    Create leerdecimal.cpp

---
+++ Examenfinal/codigos/leerdecimal.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Incluye la librería para entrada y salida.
+
+using namespace std; // Permite usar cin, cout y endl directamente.
+
+int main() { // Inicia la función principal.
+
+    double temperatura;
+
+    cout << "Ingrese la temperatura: ";
+    cin >> temperatura;
+
+    cout << temperatura << endl;
+
+    return 0;
+}
