419079eb5a6091a30476fe52474569790164116f
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:35:29 -0500

    Create leerentero.cpp

---
+++ Examenfinal/codigos/leerentero.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Incluye la librería para entrada y salida.
+
+using namespace std; // Permite usar cin, cout y endl directamente.
+
+int main() { // Inicia la función principal.
+
+    int edad; // Declara una variable entera para guardar la edad.
+
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+
+    cout << edad << endl;
+
+    return 0;
+}
