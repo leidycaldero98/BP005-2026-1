e52da423525ed378852d2a51fd73999c6d50772e
leidycaldero98 <roindira13@gmail.com>
2026-06-04 23:39:46 -0500

    Create funcionesbasicas.cpp

---
+++ Examenfinal/codigos/funcionesbasicas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    cout << sqrt(16.0) << endl;   // Raíz cuadrada
+    cout << pow(2.0, 3.0) << endl; // Potencia
+    cout << fabs(-3.5) << endl;    // Valor absoluto
+
+    return 0;
+}
